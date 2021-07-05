43ca8664d50496a95529f0914b1e26c50022000a
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2021-07-05 19:17:36 +0900

    Update notice-banner.js

--- assets/js/notice-banner.js
+++ assets/js/notice-banner.js
@@ -1,5 +1,5 @@
 function randomItem(e) {
     return e[Math.floor(Math.random() * e.length)]
 }
-var e = new Array("수도권, 7일 거리두기 단계 발표", "수도권, 3단계 격상 논의중", "수도권, 접종자도 마스크 의무", "유흥시설·학원 종사자 일제검사", "감염 재생산지수 1.2 넘어서");
+var e = new Array("수도권, 7일 거리두기 단계 발표", "4일까지 델타변이 416명 확인", "수도권, 접종자도 마스크 의무", "유흥시설·학원 종사자 일제검사", "감염 재생산지수 1.2 넘어서");
 $('#notice-news').html(randomItem(e));
