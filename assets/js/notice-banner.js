724bed4b4f844c92193dcef335562389432edea9
Ice Cream <44636839+IceCream0910@users.noreply.github.com>
2021-07-07 08:39:17 +0900

    Update notice-banner.js

--- assets/js/notice-banner.js
+++ assets/js/notice-banner.js
@@ -1,5 +1,5 @@
 function randomItem(e) {
     return e[Math.floor(Math.random() * e.length)]
 }
-var e = new Array("6개월만 확진자 1000명 넘어", "3일 기준 델타변이 416명 확인", "수도권, 접종자도 마스크 의무", "서울시 역대 최다 확진", "폭증세, 방역 강화 불가피");
+var e = new Array("상황 유지 시 최고 단계 검토", "6개월만 확진자 1000명 넘어", "3일 기준 델타변이 416명 확인", "수도권, 접종자도 마스크 의무", "서울시 역대 최다 확진", "폭증세, 방역 강화 불가피");
 $('#notice-news').html(randomItem(e));
