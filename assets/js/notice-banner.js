bbc4e15025883f0415474ec9942211101ce612f7
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2021-09-28 18:11:51 +0900

    방향성 조사 배너 추가

--- assets/js/notice-banner.js
+++ assets/js/notice-banner.js
@@ -1,7 +1,6 @@
 function randomItem(e) {
     return e[Math.floor(Math.random() * e.length)]
 }
-$('#notice-news').hide();
 
 function welcome() {
     var e = new Array("최전선에서 사투를 벌이는<br>의료진분들을 응원합니다.", "우리는 극복할 수 있어요.<br>함께 이겨낼 거예요.", "건강한 내일을 위해,<br>적극적인 동참이 필요합니다.", "백신 접종 후에도<br>방역수칙을 준수해주세요.", "일상이 다시 반짝일 때까지<br>조금만 힘내봐요, 우리.", "힘든 시기를 보내고 계신<Br>소상공인을 응원합니다.", "모든 방역 관계자분들의<br>노고에 감사드립니다.");
@@ -22,8 +21,10 @@ function isAppLatestVersion() {
     return navigator.userAgent.indexOf("android_app_" + latest_version_name) > 0;
 }
 
+
+
 if (isApp() == true) {
     if (isAppLatestVersion() == true) {
-        $('#notice-news').hide();
+        console.log("앱 최신버전");
     }
 }
